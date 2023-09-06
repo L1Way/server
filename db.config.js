f1a8e633b40c2ea468ea259cbab2cbc910f91f11
L1Way <144277263+L1Way@users.noreply.github.com>
2023-09-06 22:21:42 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cjsd1gdm702s738813vg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "7EtMv8Tmx1ptbNVmlhOGA8xCN8XL3EsA",
+    DB: "widget",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
